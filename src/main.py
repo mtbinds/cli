59786148699bb7e516431796d5fdd586c4e9ab79
Oxal <richard.planchon.pro@gmail.com>
2022-01-26 16:47:48 +0100

    test du nouveau système d'option

--- src/main.py
+++ src/main.py
@@ -1,14 +1,42 @@
 import bdd.entity as entity
 import sqlite3
+import src.bdd.orm as orm
 
 connection = sqlite3.connect('init/bdd.db')
 
-a = entity.Domaines("alo2", "alo")
+a = entity.Aptitudes(3, "alo2", "alo1")
 print(a)
 a.save(connection)
 print(["*"] == ["*"])
-l = entity.Syllabus.read(entity.Syllabus, connection,
-                         option={"syllabus_id" : {"min" : 0, "max" : 3, "equals" : True}, "syllabus_nom" : "nom",
-                                 "order" : {"syllabus_id" : "up", "syllabus_nom" : "down"}})
+l = entity.Aptitudes.read(entity.Aptitudes, connection)
+for b in l:
+    print(b)
+
+print("------------------------")
+# and entre les logic
+palu = {
+        "logic" : "or",
+        "options" : [
+            {
+                "logic": "and",
+                "options": [
+                    {
+                        "aptitudes_id": 3
+                    },
+                    {
+                        "aptitudes_nom": "alo1"
+                    }
+                ]
+            },
+            {
+                "aptitudes_nom": "alo"
+            }
+        ]
+    }
+
+print(orm.is_logic(palu))
+
+# => where (id > 0 and name = `alo`) or id = -1
+l = entity.Aptitudes.read(entity.Aptitudes, connection, option=palu, order={"aptitudes_id" : "down"})
 for b in l:
     print(b)
